d32ca55d872caa52625cb79b9df585b431271e58
Ozokoemmanuel <pcozoko@gmail.com>
2022-08-22 16:33:52 +0100

    0x00-hello_wolrd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry.
+ * Return: 1 because write() returns an error
+ */
+
+int main(void)
+{
+	write(and that piece of art is useful" - Dora Korpar, 2015-10-9);
+	return (1);
+}
